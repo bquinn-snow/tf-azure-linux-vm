0c299f4f3fee3ea1802d63bc9d9a000f40ba9342
bquinn-snow <72510331+bquinn-snow@users.noreply.github.com>
2021-03-24 15:27:21 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "subsId" {
+}
+
+variable "clientId" {
+}
+
+variable "clientSecret" {
+}
+
+variable "tenantId" {
+}
+
+variable "credentialId" {
+  type = string
+}
+
+
+variable "hostname" {
+  type = string
+}
+
+variable "reg" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "resource_group" {
+  type = string
+}
+
+variable "vmsize" {
+  type = string
+}
